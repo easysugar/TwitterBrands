231e96329d393ef3d4f6df80a0ba9bd6889c9f0b
easysugar <suprunov.vladyslav@gmail.com>
2018-04-19 23:47:54 +0300

    :art: :lipstick: :zap: add tone analyzing to api

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ app = Flask(__name__)
 
 @app.route('/api/tweets/<username>')
 def get_tweets(username):
-    result = twitter.get_texts(username, max_count=100)
-    result = list(enumerate(result))
+    result = []
+    for i, text in enumerate(twitter.get_texts(username, max_count=10)):
+        item = {'text': text}
+        for name, score in tone_analyzer.analyze(text).items():
+            item[name] = '%.2f' % score
+        result.append((i, item))
     return render_template('tweets.html', username=username, result=result)
 
 
 if __name__ == '__main__':
     config = json.load(open('resources/config.json'))
     twitter = wrappers.Twitter(config['twitter'])
+    tone_analyzer = wrappers.ToneAnalyzer(config['tone_analyzer'])
     app.run(debug=True)
